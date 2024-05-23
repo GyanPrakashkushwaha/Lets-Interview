cc66e8ae67ac9fa261765f66e6cc9645db9aa8d6
GyanPrakashkushwaha <gyanp7880@gmail.com>
2024-05-23 17:08:26 +0530

    added few more fields to the DB

--- base/models.py
+++ base/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
 class Room(models.Model):
-    # host = 
-    # topic = 
+    host = models.ForeignKey(to=User,on_delete=models.SET_NULL,null=True)
+    topic = models.ForeignKey(to=Topic,on_delete=models.SET_NULL,null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True,blank=True)
     # participants = 
@@ -13,3 +21,19 @@ class Room(models.Model):
     
     def __str__(self):
         return self.name
+    
+    
+class Message(models.Model):
+    """ This class is going to be a message creator."""
+    
+    user = models.ForeignKey(to=User,on_delete=models.CASCADE)
+        
+    # This is one to many relationship. `on_delete` parameters shows that if the room gets deleted all the messages get deleted.
+    room = models.ForeignKey(to=Room,on_delete=models.CASCADE)
+    body = models.TextField()    
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.body[:50]
+    
