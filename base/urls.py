e652a7f04049335195745fe57ae84563bec51667
GyanPrakashkushwaha <gyanp7880@gmail.com>
2024-05-23 08:58:46 +0530

    add db

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path(route='',view=views.home,name='home'),
-    path(route='room/',view=views.room,name="room")
+    path(route='room/<str:pk>/',view=views.room,name="room")
 ]
--- base/views.py
+++ base/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render , HttpResponse
 
-# Create your views here.
-def home(request):
-    return render(request=request,template_name="base/home.html")
 
 allRooms = [
     {'id': 1,'name':"interview"},
     {'id': 2,'name':"job discussion"},
     {'id': 3,'name':"About Questions"},
 ]
-def room(request):
+
+# Create your views here.
+def home(request):
+    dictToPass = {"room": allRooms}
+    
+    return render(request=request,template_name="base/home.html",context=dictToPass)
+
+def room(request,pk):
     # context dictionary
     """ to render the data into the page, I have to pass the data into the render method using a dictionary.
     I will only be able to get the data to the htlm page for rendering using the key of the dictionary. """
     
-    dictToPass = {"room": allRooms}
+    rm = None
+    for i in allRooms:
+        if i['id'] == int(pk):
+            rm = i
+            
+    context = {"rm": rm}
+    
     # in html file I can do all the things I want using the "room" key.
-    return render(request=request,template_name="base/room.html",context=dictToPass)
+    return render(request=request,template_name="base/room.html",context=context)
